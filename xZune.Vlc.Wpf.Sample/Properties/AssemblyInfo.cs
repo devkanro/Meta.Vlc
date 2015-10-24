d8ab65ad9385c85e527f29182c508aa85f6770c4
HIGAN <higan@live.cn>
2015-10-25 02:48:50 +0800

    Create nuget package

--- xZune.Vlc.Wpf.Sample/Properties/AssemblyInfo.cs
+++ xZune.Vlc.Wpf.Sample/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using xZune.Vlc;
 [assembly: AssemblyTitle("xZune.Vlc.Wpf.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("higan")]
 [assembly: AssemblyProduct("xZune.Vlc.Wpf.Sample")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -55,5 +55,5 @@ using xZune.Vlc;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("15.10.25.0")]
+[assembly: AssemblyFileVersion("15.10.25.0")]
--- xZune.Vlc.Wpf/Properties/AssemblyInfo.cs
+++ xZune.Vlc.Wpf/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 [assembly: AssemblyTitle("xZune.Vlc.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("higan")]
 [assembly: AssemblyProduct("xZune.Vlc.Wpf")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("15.10.25.0")]
+[assembly: AssemblyFileVersion("15.10.25.0")]

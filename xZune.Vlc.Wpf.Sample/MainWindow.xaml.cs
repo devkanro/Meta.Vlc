130729b1d769365901c1613f1119ce01c037dc84
George Birbilis <GitHub1@zoomicon.com>
2015-11-07 19:57:06 +0200

    - Sample app: Better layout for buttons, added button paddings using styles - Sample app: Cleaned up XAML and code behind, named the various event handlers

--- xZune.Vlc.Wpf.Sample/MainWindow.xaml.cs
+++ xZune.Vlc.Wpf.Sample/MainWindow.xaml.cs
@@ -1,67 +1,84 @@
-﻿using System;
+﻿//Project: xZune.Vlc (https://github.com/higankanshi/xZune.Vlc)
+//Filename: MainWindow.xaml.cs
+//Version: 20151107
+
+//Note: can find VLC stream URLs for testing at http://www.vlchistory.eu.pn/
+
+using System;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace xZune.Vlc.Wpf.Sample
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : Window
+
+  public partial class MainWindow : Window
+  {
+
+    #region --- Initialization ---
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        protected override void OnClosing(CancelEventArgs e)
-        {
-            base.OnClosing(e);
-            Player.Dispose();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Player.Stop();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Player.PauseOrResume();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            Player.Play();
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            Uri uri = new Uri(path.Text);
-            //String pathString = path.Text;
-
-            Player.BeginStop((ar) =>
-            {
-                Player.LoadMedia(uri);
-                Player.Play();
-            });
-        }
-
-        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            switch ((sender as ComboBox).SelectedIndex)
-            {
-                case 0:
-                    Player.AspectRatio = AspectRatio.Default;
-                    break;
-                case 1:
-                    Player.AspectRatio = AspectRatio._16_9;
-                    break;
-                case 2:
-                    Player.AspectRatio  = AspectRatio._4_3;
-                    break;
-            }
-        }
+      InitializeComponent();
     }
+
+    #endregion
+
+    #region --- Cleanup ---
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+      base.OnClosing(e);
+      Player.Dispose();
+    }
+
+    #endregion
+
+    #region --- Events ---
+
+    private void Load_Click(object sender, RoutedEventArgs e)
+    {
+      Uri uri = new Uri(path.Text);
+      //String pathString = path.Text;
+
+      Player.BeginStop((ar) =>
+      {
+        Player.LoadMedia(uri);
+        Player.Play();
+      });
+    }
+
+    private void Play_Click(object sender, RoutedEventArgs e)
+    {
+      Player.Play();
+    }
+    private void Pause_Click(object sender, RoutedEventArgs e)
+    {
+      Player.PauseOrResume();
+    }
+
+    private void Stop_Click(object sender, RoutedEventArgs e)
+    {
+      Player.Stop();
+    }
+
+    private void AspectRatio_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+      switch ((sender as ComboBox).SelectedIndex)
+      {
+        case 0:
+          Player.AspectRatio = AspectRatio.Default;
+          break;
+        case 1:
+          Player.AspectRatio = AspectRatio._16_9;
+          break;
+        case 2:
+          Player.AspectRatio = AspectRatio._4_3;
+          break;
+      }
+    }
+
+    #endregion
+
+  }
 }
